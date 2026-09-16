a41ebdfb3f87d455c4e97bf5f2f50aca26a3517b
Luis Fuentes Urra <luisfuentesurra@gmail.com>
2026-09-16 17:20:06 -0400

    Corregir categorias de publicaciones y automatizar aparicion de noticias aprobadas

--- app.py
+++ app.py
@@ -165,7 +165,7 @@ with tab7:
     
     clave = st.text_input("Ingrese la clave de administrador:", type="password")
     
-    if clave == "1234":
+    if clave == "bosque2026":
         st.success("Acceso concedido al Panel de Control.")
         
         # --- A. EDITAR MENSAJE DIARIO ---
