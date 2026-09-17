bc285e95423d43102480a25c67172e0b46864ef6
Luis Fuentes Urra <luisfuentesurra@gmail.com>
2026-09-16 20:46:01 -0400

    Habilitar panel de administracion amigable para equipo editorial

--- app.py
+++ app.py
@@ -174,7 +174,7 @@ with tab7:
     
     clave = st.text_input("Ingrese la clave de administrador:", type="password")
     
-    if clave == "1234":  # Recuerde colocar aquí su clave personalizada
+    if clave == "bosque2026"
         st.success("Acceso concedido al Panel Editorial.")
         
         # --- A. EDITAR MENSAJE DIARIO ---
